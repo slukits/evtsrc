5e00584f14f372afcf5a259a90290e23222b1904
Stephan Lukits <stephan.lukits@gmail.com>
2022-12-11 13:33:14 +0200

    add: minimal event source server for first test

---
+++ evtsrc.py
@@ -0,0 +1,39 @@
+#!/home/goedel/python/evtsrc/.env/bin/python
+# -*- coding: utf-8 -*-
+#
+# This file is part of evtsrc released under the MIT license.
+# See the NOTICE for more information.
+
+"""
+evtsrc is an event source storing a software system's state changing
+events and broadcasts them to event listeners respectively provides them
+to client request.  An event becomes part of an event source if it is
+posted by a software component to evtsrc's rest api, or if it is emitted
+by an event-generator which is posted to an event source by a software
+component.
+"""
+
+from http import HTTPStatus
+from collections import namedtuple
+import json
+
+OK = "{} {}".format(HTTPStatus.OK.value, HTTPStatus.OK.phrase)
+BAD = "{} {}".format(
+    HTTPStatus.BAD_REQUEST.value, HTTPStatus.BAD_REQUEST.phrase)
+
+hdr = namedtuple('hdr', 'name value')
+HDR_CT_JSON = hdr("Content-Type", "application/json")
+
+
+def hdr_content_len(data): return hdr("Content-Length", str(len(data)))
+
+
+def evtsrc(environ, start_respond):
+    """evtsrc is the entry point for the event source server which
+    evaluates given wsgi (PEP 3333) environment environ for a mandatory
+    json post and leverages start_respond to deliver a mandatory json
+    response.
+    """
+    data = json.dumps({'evtsrc': 'v0.0.0'}).encode('utf-8')
+    start_respond(OK, [HDR_CT_JSON, hdr_content_len(data)])
+    return iter([data])
